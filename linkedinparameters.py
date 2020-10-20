8327587e7d8682d01987219dc40106f9ea5be0c2
rayane-b <44729925+rayane-b@users.noreply.github.com>
2020-10-19 23:41:28 -0400

    Add files via upload

---
+++ linkedinparameters.py
@@ -0,0 +1,4 @@
+file_name = 'results_file.csv'
+linkedin_username = '' #ENTER YOUR USERNAME HERE, YES AS A STRING
+linkedin_password = '' #ENTER YOUR PASSWORD HERE, YES AS A STRING
+chromedriver_path = '' #ENTER THE PATH TO CHROMEDRIVER HERE, YES AS A STRING
